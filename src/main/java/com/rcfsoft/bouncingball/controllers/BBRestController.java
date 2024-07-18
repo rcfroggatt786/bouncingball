3315d62478e62745cbbda2a5e37cd74a4923f1ac
Russell Froggatt <russellfroggatt@gmail.com>
2024-07-18 23:32:29 +0100

    Messing around

--- src/main/java/com/rcfsoft/bouncingball/controllers/BBRestController.java
+++ src/main/java/com/rcfsoft/bouncingball/controllers/BBRestController.java
@@ -1,23 +1,23 @@
 package com.rcfsoft.bouncingball.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.ByteBuffer;
 
 @RestController
 public class BBRestController {
-    @GetMapping(path = "/api/v1", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    @ResponseBody
-    public byte[] bbGet(){
+    @GetMapping(path = "/api/v1", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] bbGet(HttpServletResponse response){
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
         byte[] rawBuffer = new byte[1024];
         for (int i=0; i<1024; i++ ) {
             byteBuffer.put(i, (byte) (256 * Math.random()));
         }
         byteBuffer.get(rawBuffer);
+        response.setHeader("Content-Disposition", "inline; filename=data.png");
         return rawBuffer;
     }
 }
