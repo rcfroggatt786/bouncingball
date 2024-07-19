f6704a190010d4b8a776afaabde3c9065626cfe9
Russell Froggatt <russellfroggatt@gmail.com>
2024-07-19 15:17:25 +0100

    Achieved 600fps + on MacBook localhost

--- src/main/java/com/rcfsoft/bouncingball/controllers/BBRestController.java
+++ src/main/java/com/rcfsoft/bouncingball/controllers/BBRestController.java
@@ -11,20 +11,24 @@ import java.nio.ByteBuffer;
 
 @RestController
 public class BBRestController {
-
-    private byte[] getRawBuffer() {
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
-        byte[] rawBuffer = new byte[1024];
-        for (int i=0; i<1024; i++ ) {
-            byteBuffer.put(i, (byte) (256 * Math.random()));
+    private final byte[] byteBuffer = new byte[800*600*4];
+    private int f = 0;
+    private byte[] getByteBuffer() {
+        for(int y=0; y<600; y++) {
+            for (int x = 0; x < 800; x++) {
+                byteBuffer[(x + y * 800) * 4    ] = (byte) ((x + f) % 256);
+                byteBuffer[(x + y * 800) * 4 + 1] = (byte) ((y + 3*f) % 256);
+                byteBuffer[(x + y * 800) * 4 + 2] = (byte) ((x + y + 2*f) % 256);
+                byteBuffer[(x + y * 800) * 4 + 3] = (byte) 255;
+            }
         }
-        byteBuffer.rewind();
-        byteBuffer.get(rawBuffer);
-        return rawBuffer;
+        f++;
+
+        return byteBuffer;
     }
 
     @GetMapping(path = "/api/v1", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public ResponseEntity<byte[]> bbGet() {
-        return ResponseEntity.ok().header("Content-Disposition", "inline; name=test.raw").body(getRawBuffer());
+        return ResponseEntity.ok().header("Content-Disposition", "inline; name=test.raw").body(getByteBuffer());
     }
 }
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,34 @@
+setup();
+
+function setup() {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    main_loop(context);
+}
+
+async function main_loop(context) {
+    let frame = 0;
+    let startTime = performance.now();
+    const fpsElement = document.getElementById("fps");
+    while(true) {
+        let response = await fetch("/api/v1");
+        if (!response.ok) {
+            console.error("Could not fetch image data");
+        }
+        else {
+            response.arrayBuffer().then(
+                (arrayBuffer) => {
+                    context.putImageData(new ImageData(new Uint8ClampedArray(arrayBuffer), 800, 600), 0, 0);
+                }
+            );
+        }
+        frame++;
+        if(frame == 120) {
+            let currTime = performance.now();
+            let fps = 120000.0 / (currTime - startTime);
+            startTime = currTime;
+            frame = 0;
+            fpsElement.innerHTML = `${fps.toFixed(2)} fps`;
+        }
+    }
+}
