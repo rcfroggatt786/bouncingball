366509c32ee20644625df9eecae32afe5dc0b75a
Russell Froggatt <russellfroggatt@gmail.com>
2024-07-19 17:45:16 +0100

    Background buffer

--- src/main/java/com/rcfsoft/bouncingball/controllers/BBRestController.java
+++ src/main/java/com/rcfsoft/bouncingball/controllers/BBRestController.java
@@ -1,29 +1,45 @@
 package com.rcfsoft.bouncingball.controllers;
 
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.ByteBuffer;
-
 @RestController
 public class BBRestController {
-    private final byte[] byteBuffer = new byte[800*600*4];
-    private int f = 0;
-    private byte[] getByteBuffer() {
-        for(int y=0; y<600; y++) {
-            for (int x = 0; x < 800; x++) {
-                byteBuffer[(x + y * 800) * 4    ] = (byte) ((x + f) % 256);
-                byteBuffer[(x + y * 800) * 4 + 1] = (byte) ((y + 3*f) % 256);
-                byteBuffer[(x + y * 800) * 4 + 2] = (byte) ((x + y + 2*f) % 256);
-                byteBuffer[(x + y * 800) * 4 + 3] = (byte) 255;
+    private final int xSize = 800;
+    private final int ySize = 600;
+    private final int bytesPerPixel = 4;
+    private final int bufferSize = xSize * ySize * bytesPerPixel;
+    private final byte[] backgroundBuffer = initBackground();
+    private final byte[] byteBuffer = new byte[bufferSize];
+    private float xPos, yPos, xVel, yVel;
+
+    private byte[] initBackground() {
+        byte[] backBuffer = new byte[bufferSize];
+        for(int y = 0; y < ySize; y++) {
+            for (int x = 0; x < xSize; x++) {
+                int offset = (x + y * xSize) << 2;
+                backBuffer[offset++] = (byte) 0;
+                backBuffer[offset++] = (byte) 0;
+                backBuffer[offset++] = (byte) 0;
+                backBuffer[offset  ] = (byte) 255;
             }
         }
-        f++;
+        return backBuffer;
+    }
 
+    private byte[] getByteBuffer() {
+        System.arraycopy(backgroundBuffer, 0, byteBuffer, 0, bufferSize );
+        for(int y=0; y<600; y+=33) {
+            for (int x = 0; x < 800; x+=17) {
+                int offset = (x + y * 800) << 2;
+//                byteBuffer[offset++] = (byte) 123;
+//                byteBuffer[offset++] = (byte) 234;
+//                byteBuffer[offset++] = (byte) 77;
+//                byteBuffer[offset] = (byte) 255;
+            }
+        }
         return byteBuffer;
     }
 
