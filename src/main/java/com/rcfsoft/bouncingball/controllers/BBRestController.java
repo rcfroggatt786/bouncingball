185c7646b94dd41bdcec4c1d3806182669eeab43
Russell Froggatt <russellfroggatt@gmail.com>
2024-07-19 01:59:13 +0100

    Time for bed

--- src/main/java/com/rcfsoft/bouncingball/controllers/BBRestController.java
+++ src/main/java/com/rcfsoft/bouncingball/controllers/BBRestController.java
@@ -1,7 +1,9 @@
 package com.rcfsoft.bouncingball.controllers;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +11,20 @@ import java.nio.ByteBuffer;
 
 @RestController
 public class BBRestController {
-    @GetMapping(path = "/api/v1", produces = MediaType.IMAGE_PNG_VALUE)
-    public byte[] bbGet(HttpServletResponse response){
+
+    private byte[] getRawBuffer() {
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
         byte[] rawBuffer = new byte[1024];
         for (int i=0; i<1024; i++ ) {
             byteBuffer.put(i, (byte) (256 * Math.random()));
         }
+        byteBuffer.rewind();
         byteBuffer.get(rawBuffer);
-        response.setHeader("Content-Disposition", "inline; filename=data.png");
         return rawBuffer;
+    }
+
+    @GetMapping(path = "/api/v1", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<byte[]> bbGet() {
+        return ResponseEntity.ok().header("Content-Disposition", "inline; name=test.raw").body(getRawBuffer());
     }
 }
